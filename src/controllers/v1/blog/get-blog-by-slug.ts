4f2138ea6acf0178d540a663bc88c168f812c04e
ifaamrillah <amrillah.work@gmail.com>
2025-06-19 17:47:26 +0700

    feat: add get blog by slug controller

---
+++ src/controllers/v1/blog/get-blog-by-slug.ts
@@ -0,0 +1,62 @@
+import type { Request, Response } from 'express';
+
+import config from '@/config';
+
+import Blog from '@/models/blog';
+import User from '@/models/user';
+
+import { logger } from '@/lib/winston';
+
+const getBlogBySlug = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = req.userId;
+    const slug = req.params.slug;
+
+    const user = await User.findById(userId).select('role').lean().exec();
+
+    const blog = await Blog.findOne({ slug })
+      .select('-banner.publicId -__v')
+      .populate('author', '-createdAt -updatedAt -__v')
+      .lean()
+      .exec();
+
+    if (!blog) {
+      res.status(404).json({
+        code: 'NotFound',
+        message: 'Blog not found',
+      });
+      return;
+    }
+
+    // Show only the published post to a normal user
+    if (user?.role === 'user' && blog?.status === 'draft') {
+      res.status(403).json({
+        code: 'AuthorizationError',
+        message: 'Access denied, insufficient permissions',
+      });
+
+      logger.warn('A user tried to access a draft blog', {
+        userId,
+        blog,
+      });
+
+      return;
+    }
+
+    res.status(200).json({
+      blog,
+    });
+
+    logger.info(`All users fetched successfully.`);
+  } catch (error) {
+    res.status(500).json({
+      code: 'ServerError',
+      message: 'Internal server error',
+      error: error,
+    });
+
+    logger.error('Error getting blog by slug:', error);
+  }
+};
+
+export default getBlogBySlug;
