fdea33df9bdbc45a9f43f819c03ac92a537cdb5a
ifaamrillah <amrillah.work@gmail.com>
2025-06-27 18:52:10 +0700

    feat: delete blogs related to the deleted user

--- src/controllers/v1/user/delete-current-user.ts
+++ src/controllers/v1/user/delete-current-user.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from 'express';
+import { v2 as cloudinary } from 'cloudinary';
 
 import User from '@/models/user';
+import Blog from '@/models/blog';
 
 import { logger } from '@/lib/winston';
 
@@ -11,6 +13,25 @@ const deleteCurrentUser = async (
   const userId = req.userId;
 
   try {
+    const blogs = await Blog.find({ author: userId })
+      .select('banner.publicId')
+      .lean()
+      .exec();
+    const publicIds = blogs.map(({ banner }) => banner.publicId);
+
+    await cloudinary.api.delete_resources(publicIds);
+
+    logger.info('Multiple blog banner deleted from Cloudinary', {
+      publicIds,
+    });
+
+    await Blog.deleteMany({ author: userId });
+
+    logger.info('Multiple blogs deleted', {
+      userId,
+      blogs,
+    });
+
     await User.deleteOne({ _id: userId });
 
     res.sendStatus(204);
--- src/controllers/v1/user/delete-user-by-id.ts
+++ src/controllers/v1/user/delete-user-by-id.ts
@@ -1,12 +1,33 @@
 import type { Request, Response } from 'express';
+import { v2 as cloudinary } from 'cloudinary';
 
 import User from '@/models/user';
+import Blog from '@/models/blog';
 
 import { logger } from '@/lib/winston';
 
 const deleteUserById = async (req: Request, res: Response): Promise<void> => {
   try {
     const userId = req.params.userId;
+
+    const blogs = await Blog.find({ author: userId })
+      .select('banner.publicId')
+      .lean()
+      .exec();
+    const publicIds = blogs.map(({ banner }) => banner.publicId);
+
+    await cloudinary.api.delete_resources(publicIds);
+
+    logger.info('Multiple blog banner deleted from Cloudinary', {
+      publicIds,
+    });
+
+    await Blog.deleteMany({ author: userId });
+
+    logger.info('Multiple blogs deleted', {
+      userId,
+      blogs,
+    });
 
     await User.deleteOne({ _id: userId });
 
